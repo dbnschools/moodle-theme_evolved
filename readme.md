6e04e0e93a30c45f49a7755c1e72b812b373685e
kennibc <kennibc@dearbornschools.org>
2014-07-28 15:36:51 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+evolve-D
+======================
+
+Evolve-D(earborn) encapsulates simplicity, function, and style as it unites many notable Moodle theme features into an evolved "More" theme. Evolve-D applies famed theme elements such as Google Fonts, My Courses dropdown, block icons, and a variety of other enhancements made popular in Essential and Aardvark Themes.  The evolve-D theme's appeal comes from it's simple 2 column design, muted colors, and focus on displaying content and minimizing distractions.
+
+[Dearborn Public Schools](http://dearbornschools.org) is a K-12 school distict in Dearborn, Michigan. We designed and use evolve-D on our Moodle site.
+
+Special thanks to [Aardvark](https://moodle.org/plugins/view.php?plugin=theme_aardvark) and [Essential](https://moodle.org/plugins/view.php?plugin=theme_essential) theme developers Shaun Daubney and Julian Ridden for thier work which is replicated in part here.
+
+evolve-D v1 "Tin Lizzy"
+======================
+Much like the Model T the evolve-D theme has interchangeable parts from other themes that are bolted onto the More theme to improve useability, functionality, style, and appeal.
+
+Tin Lizzy Release Notes:
+ - Initial release
+ - Enhancement of core theme "More"
+ - Works with Moodle 2.7+
+ - Matching Mahara ePortfolio theme Download [dbnmahara](https://github.com/kennibc/dbnmahara)
+ - Google Font Selector
+ - Custom logo
+ - Custom colors and backgrounds
+ - Smarter navigation with My Courses and My Dashboard drop down menus
+ - Bootstrap responsive design
+ - Stylish block icons to enhance visuals
+ - Can be changed from 2 column to 3 column by simply modifying the config.php file to change "columns2rt.php" to "columns3.php"
---
+++ settings.php
@@ -0,0 +1,246 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for evolved details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme evolved temp.
+ *
+ * Each setting that is defined in the parent theme Clean should be
+ * defined here too, and use the exact same config name. The reason
+ * is that theme_evolved does not define any layout files to re-use the
+ * ones from theme_clean. But as those layout files use the function
+ * {@link theme_clean_get_html_for_temp} that belong to Clean,
+ * we have to make sure it works as expected by having the same temp
+ * in our theme.
+ *
+ * @see        theme_clean_get_html_for_temp
+ * @package    theme_evolved
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$settings = null;
+
+defined('MOODLE_INTERNAL') || die;
+
+
+	$ADMIN->add('themes', new admin_category('theme_evolved', 'evolve-D'));
+
+	// "geneictemp" settingpage
+    $temp = new admin_settingpage('theme_evolved_generic',  get_string('geneicsettings', 'theme_evolved'));
+	// Font Selector.
+    $name = 'theme_evolved/fontselect';
+    $title = get_string('fontselect' , 'theme_evolved');
+    $description = get_string('fontselectdesc', 'theme_evolved');
+    $default = '1';
+    $choices = array(
+    	'1'=>'Oswald & PT Sans', 
+    	'2'=>'Lobster & Cabin', 
+    	'3'=>'Raleway & Goudy', 
+    	'4'=>'Allerta & Crimson Text', 
+    	'5'=>'Arvo & PT Sans',
+    	'6'=>'Dancing Script & Josefin Sans',
+    	'7'=>'Allan & Cardo',
+    	'8'=>'Molengo & Lekton',
+    	'9'=>'Droid Serif & Droid Sans',
+    	'10'=>'Corbin & Nobile',
+    	'11'=>'Ubuntu & Vollkorn',
+    	'12'=>'Bree Serif & Open Sans', 
+    	'13'=>'Bevan & Pontano Sans', 
+    	'14'=>'Abril Fatface & Average', 
+    	'15'=>'Playfair Display and Muli', 
+    	'16'=>'Sansita One & Kameron',
+    	'17'=>'Istok Web & Lora',
+    	'18'=>'Pacifico & Arimo',
+    	'19'=>'Nixie One & Ledger',
+    	'20'=>'Cantata One & Imprima',
+    	'21'=>'Rancho & Gudea',
+    	'22'=>'DISABLE Google Fonts');
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Invert Navbar to dark background.
+    $name = 'theme_evolved/invert';
+    $title = get_string('invert', 'theme_evolved');
+    $description = get_string('invertdesc', 'theme_evolved');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 1);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Logo file setting.
+    $name = 'theme_evolved/logo';
+    $title = get_string('logo','theme_evolved');
+    $description = get_string('logodesc', 'theme_evolved');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_evolved/customcss';
+    $title = get_string('customcss', 'theme_evolved');
+    $description = get_string('customcssdesc', 'theme_evolved');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Footnote setting.
+    $name = 'theme_evolved/footnote';
+    $title = get_string('footnote', 'theme_evolved');
+    $description = get_string('footnotedesc', 'theme_evolved');
+    $default = '';
+    $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    $ADMIN->add('theme_evolved', $temp);
+
+    $temp = new admin_settingpage('theme_evolved_themecolors',  get_string('themecolorsettings', 'theme_evolved'));
+
+ // @textColor setting.
+    $name = 'theme_evolved/textcolor';
+    $title = get_string('textcolor', 'theme_evolved');
+    $description = get_string('textcolor_desc', 'theme_evolved');
+    $default = '#3d3d3d';
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // @linkColor setting.
+    $name = 'theme_evolved/linkcolor';
+    $title = get_string('linkcolor', 'theme_evolved');
+    $description = get_string('linkcolor_desc', 'theme_evolved');
+    $default = '#415FFB';
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+	 // Main content background color.
+    $name = 'theme_evolved/contentbackground';
+    $title = get_string('contentbackground', 'theme_evolved');
+    $description = get_string('contentbackground_desc', 'theme_evolved');
+    $default = '#FFFFFF';
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Secondary background color.
+    $name = 'theme_evolved/secondarybackground';
+    $title = get_string('secondarybackground', 'theme_evolved');
+    $description = get_string('secondarybackground_desc', 'theme_evolved');
+    $default = '#CAD9E8';
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+    // @bodyBackground setting.
+    $name = 'theme_evolved/bodybackground';
+    $title = get_string('bodybackground', 'theme_evolved');
+    $description = get_string('bodybackground_desc', 'theme_evolved');
+    $default = '#FFFBFF';
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Background image setting.
+    $name = 'theme_evolved/backgroundimage';
+    $title = get_string('backgroundimage', 'theme_evolved');
+    $description = get_string('backgroundimage_desc', 'theme_evolved');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'backgroundimage');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Background repeat setting.
+    $name = 'theme_evolved/backgroundrepeat';
+    $title = get_string('backgroundrepeat', 'theme_evolved');
+    $description = get_string('backgroundrepeat_desc', 'theme_evolved');;
+    $default = 'repeat-x';
+    $choices = array(
+        '0' => get_string('default'),
+        'repeat' => get_string('backgroundrepeatrepeat', 'theme_evolved'),
+        'repeat-x' => get_string('backgroundrepeatrepeatx', 'theme_evolved'),
+        'repeat-y' => get_string('backgroundrepeatrepeaty', 'theme_evolved'),
+        'no-repeat' => get_string('backgroundrepeatnorepeat', 'theme_evolved'),
+    );
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Background position setting.
+    $name = 'theme_evolved/backgroundposition';
+    $title = get_string('backgroundposition', 'theme_evolved');
+    $description = get_string('backgroundposition_desc', 'theme_evolved');
+    $default = 'left_bottom';
+    $choices = array(
+        '0' => get_string('default'),
+        'left_top' => get_string('backgroundpositionlefttop', 'theme_evolved'),
+        'left_center' => get_string('backgroundpositionleftcenter', 'theme_evolved'),
+        'left_bottom' => get_string('backgroundpositionleftbottom', 'theme_evolved'),
+        'right_top' => get_string('backgroundpositionrighttop', 'theme_evolved'),
+        'right_center' => get_string('backgroundpositionrightcenter', 'theme_evolved'),
+        'right_bottom' => get_string('backgroundpositionrightbottom', 'theme_evolved'),
+        'center_top' => get_string('backgroundpositioncentertop', 'theme_evolved'),
+        'center_center' => get_string('backgroundpositioncentercenter', 'theme_evolved'),
+        'center_bottom' => get_string('backgroundpositioncenterbottom', 'theme_evolved'),
+    );
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    // Background fixed setting.
+    $name = 'theme_evolved/backgroundfixed';
+    $title = get_string('backgroundfixed', 'theme_evolved');
+    $description = get_string('backgroundfixed_desc', 'theme_evolved');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+    $ADMIN->add('theme_evolved', $temp);
+
+    /* Custom Menu temp */
+    $temp = new admin_settingpage('theme_evolved_custommenu', get_string('custommenuheading', 'theme_evolved'));
+
+    // Toggle courses display in custommenu.
+    $name = 'theme_evolved/displaymycourses';
+    $title = get_string('displaymycourses', 'theme_evolved');
+    $description = get_string('displaymycoursesdesc', 'theme_evolved');
+    $default = true;
+    $setting = new admin_setting_configcheckbox($name, $title, $description, $default, true, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+    
+    // Set terminology for dropdown course list
+	$name = 'theme_evolved/mycoursetitle';
+	$title = get_string('mycoursetitle','theme_evolved');
+	$description = get_string('mycoursetitledesc', 'theme_evolved');
+	$default = 'course';
+	$choices = array(
+		'course' => get_string('mycourses', 'theme_evolved'),
+		'unit' => get_string('myunits', 'theme_evolved'),
+		'class' => get_string('myclasses', 'theme_evolved'),
+		'module' => get_string('mymodules', 'theme_evolved')
+	);
+	$setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+	$setting->set_updatedcallback('theme_reset_all_caches');
+	$temp->add($setting);
+
+    // Toggle dashboard display in custommenu.
+    $name = 'theme_evolved/displaymydashboard';
+    $title = get_string('displaymydashboard', 'theme_evolved');
+    $description = get_string('displaymydashboarddesc', 'theme_evolved');
+    $default = true;
+    $setting = new admin_setting_configcheckbox($name, $title, $description, $default, true, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $temp->add($setting);
+
+$ADMIN->add('theme_evolved', $temp);
